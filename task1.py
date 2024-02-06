c90531c577ced5c4ef3ebd2c4c64d452017f18fd
shehan2000 <maliths.20@cse.mrt.ac.lk>
2024-02-06 15:03:28 +0530

    task1 completed

---
+++ task1.py
@@ -0,0 +1,46 @@
+import numpy as np 
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+# a=np.array([5,10,45,1,-10])
+# print(type(a))
+# print(a.shape)
+
+# for i in range(0,len(a)):
+#     print(a[i])
+
+# for i in a:
+#     print(i)
+# aa=a<40
+# print(aa)      
+
+# aaa=a*aa
+# print(aaa)
+# img = cv.imread("D:/self/OCV/Open-CV/Part 1/part1Materials/messi.jpg",1)
+# img= cv.pyrDown(img)
+
+# b,g,r=cv.split(img)
+
+
+
+# plt.imshow(img)
+
+# cv.namedWindow("b",cv.WINDOW_NORMAL)
+# cv.imshow("b",b)
+
+
+# cv.namedWindow("g",cv.WINDOW_NORMAL)
+# cv.imshow("g",g)
+
+
+
+
+# cv.namedWindow("r",cv.WINDOW_NORMAL)
+# cv.imshow("r",r)
+# plt.show()
+
+
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+
